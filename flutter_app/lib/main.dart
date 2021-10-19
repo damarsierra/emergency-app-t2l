a89500d0ab994756ef9f79b5d30d3ffab9b39034
Damar Sierra <dsierra7769@floridapoly.edu>
2021-10-19 16:00:24 -0400

    Removed some packages.

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:emergency_app_flutter/routes.dart';
 import 'package:emergency_app_flutter/theme/style.dart';
