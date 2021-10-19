7c08ca4d8a13f692ae9c273fef60f9a6b7461bde
Damar Sierra <dsierra7769@floridapoly.edu>
2021-10-19 16:00:56 -0400

    Added style.dart package. Removed local styles.

--- flutter_app/lib/screens/gps_perms/gps_perms_screen.dart
+++ flutter_app/lib/screens/gps_perms/gps_perms_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:emergency_app_flutter/theme/style.dart';
+
 class GPSPermsScreen extends StatefulWidget {
   const GPSPermsScreen({Key? key}) : super(key: key);
 
@@ -10,60 +12,12 @@ class GPSPermsScreen extends StatefulWidget {
 class _GPSPermsScreenState extends State<GPSPermsScreen> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-    final ButtonStyle bstyle = OutlinedButton.styleFrom(
-      backgroundColor: const Color.fromRGBO(226, 226, 226, 30), // background
-      primary: Colors.black87, //text (foreground)
-      textStyle: const TextStyle(
-          fontFamily: 'RedHatText',
-          fontWeight: FontWeight.w500,
-          fontSize: 24,
-          leadingDistribution: TextLeadingDistribution.even),
-      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
-      minimumSize: const Size(150, 45),
-      maximumSize: const Size.fromWidth(270),
-      side: const BorderSide(color: Colors.black87, width: 2),
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(12)),
-      ),
-    );
-    const BoxDecoration dbstyle = BoxDecoration(
-      boxShadow: [
-        BoxShadow(
-          color: Color.fromRGBO(37, 37, 37, 25),
-          spreadRadius: 1,
-          blurRadius: 4,
-          offset: Offset(0, 4),
-        )
-      ],
-    );
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Row(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           children: <Widget>[
             Flexible(
               child: Column(
@@ -84,9 +38,9 @@ class _GPSPermsScreenState extends State<GPSPermsScreen> {
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 8),
                         child: Container(
-                          decoration: dbstyle,
+                          decoration: boxDecoration(),
                           child: OutlinedButton(
-                            style: bstyle,
+                            style: buttonStyle(),
                             onPressed: () {
                               // Validate will return true if the form is valid, or false if
                               // the form is invalid.
@@ -104,9 +58,9 @@ class _GPSPermsScreenState extends State<GPSPermsScreen> {
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 8),
                         child: Container(
-                          decoration: dbstyle,
+                          decoration: boxDecoration(),
                           child: OutlinedButton(
-                            style: bstyle,
+                            style: buttonStyle(),
                             onPressed: () {
                               // Validate will return true if the form is valid, or false if
                               // the form is invalid.
--- flutter_app/lib/screens/home/home_screen.dart
+++ flutter_app/lib/screens/home/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:emergency_app_flutter/theme/style.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -17,55 +19,11 @@ class _HomeScreenState extends State<HomeScreen> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-    final ButtonStyle bstyle = OutlinedButton.styleFrom(
-      backgroundColor: const Color.fromRGBO(226, 226, 226, 30),
-      // background
-      primary: Colors.black87,
-      //text (foreground)
-      textStyle: const TextStyle(
-          fontFamily: 'RedHatText',
-          fontWeight: FontWeight.w500,
-          fontSize: 24,
-          leadingDistribution: TextLeadingDistribution.even),
-      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
-      minimumSize: const Size(150, 45),
-      maximumSize: const Size.fromWidth(270),
-      side: const BorderSide(color: Colors.black87, width: 2),
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(12)),
-      ),
-    );
-    const BoxDecoration dbstyle = BoxDecoration(
-      boxShadow: [
-        BoxShadow(
-          color: Color.fromRGBO(37, 37, 37, 25),
-          spreadRadius: 1,
-          blurRadius: 4,
-          offset: Offset(0, 4),
-        )
-      ],
-    );
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Row(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           children: <Widget>[
             Flexible(
               child: Column(
@@ -89,7 +47,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     padding:
                     const EdgeInsets.symmetric(vertical: 8, horizontal: 32),
                     child: Container(
-                      decoration: dbstyle,
+                      decoration: boxDecoration(),
                       child: Form(
                         key: _formKey,
                         child: TextFormField(
@@ -115,9 +73,9 @@ class _HomeScreenState extends State<HomeScreen> {
                   Padding(
                     padding: const EdgeInsets.only(top: 8, bottom: 8),
                     child: Container(
-                      decoration: dbstyle,
+                      decoration: boxDecoration(),
                       child: OutlinedButton(
-                        style: bstyle,
+                        style: buttonStyle(),
                         onPressed: () {
                           // Validate will return true if the form is valid, or false if
                           // the form is invalid.
@@ -142,9 +100,9 @@ class _HomeScreenState extends State<HomeScreen> {
                   Padding(
                     padding: const EdgeInsets.only(top: 60),
                     child: Container(
-                      decoration: dbstyle,
+                      decoration: boxDecoration(),
                       child: OutlinedButton(
-                        style: bstyle,
+                        style: buttonStyle(),
                         onPressed: () {
                           // Validate will return true if the form is valid, or false if
                           // the form is invalid.
