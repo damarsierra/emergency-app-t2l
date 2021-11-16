b5bab903a6dc6ae8678da6b70b00f14fb5154841
Damar Sierra <dsierra7769@floridapoly.edu>
2021-11-16 10:12:29 -0500

    added new user route

--- flutter_app/lib/routes.dart
+++ flutter_app/lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/widgets.dart';
 import 'package:emergency_app_flutter/screens/home/home_screen.dart';
 import 'package:emergency_app_flutter/screens/gps_perms/gps_perms_screen.dart';
+import 'package:emergency_app_flutter/screens/new_user/new_user.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   "/": (BuildContext context) => const HomeScreen(),
   "/GPSPerms": (BuildContext context) => const GPSPermsScreen(),
+  "/NewUser": (BuildContext context) => const NewUser(),
 };
