feb091f1656dac578c64f77b1b3e3075d60b1312
Damar Sierra <dsierra7769@floridapoly.edu>
2021-11-28 16:17:26 -0500

    Added all common crime routes

--- flutter_app/lib/routes.dart
+++ flutter_app/lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:emergency_app_flutter/screens/success_submit/success_submit_screen.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:emergency_app_flutter/screens/home/home_screen.dart';
 import 'package:emergency_app_flutter/screens/gps_perms/gps_perms_screen.dart';
 import 'package:emergency_app_flutter/screens/emergency_profile/emergency_profile_screen.dart';
+
 import 'package:emergency_app_flutter/screens/stalking/armed_stalking_screen.dart';
 import 'package:emergency_app_flutter/screens/stalking/doing_currently_screen.dart';
 import 'package:emergency_app_flutter/screens/stalking/have_done_screen.dart';
@@ -12,6 +14,17 @@ import 'package:emergency_app_flutter/screens/stalking/recognize_screen.dart';
 import 'package:emergency_app_flutter/screens/stalking/stalking_screen.dart';
 import 'package:emergency_app_flutter/screens/stalking/with_anyone_screen.dart';
 import 'package:emergency_app_flutter/screens/select_crime/select_crime_screen.dart';
+
+import 'package:emergency_app_flutter/screens/common_crimes/when_it_happened_screen.dart';
+import 'package:emergency_app_flutter/screens/common_crimes/what_happened_screen.dart';
+import 'package:emergency_app_flutter/screens/common_crimes/where_it_happened_screen.dart';
+import 'package:emergency_app_flutter/screens/common_crimes/see_perpetrator_screen.dart';
+import 'package:emergency_app_flutter/screens/common_crimes/wearing_screen.dart';
+import 'package:emergency_app_flutter/screens/common_crimes/man_or_woman_screen.dart';
+import 'package:emergency_app_flutter/screens/common_crimes/age_screen.dart';
+import 'package:emergency_app_flutter/screens/common_crimes/height_screen.dart';
+import 'package:emergency_app_flutter/screens/common_crimes/race_screen.dart';
+import 'package:emergency_app_flutter/screens/common_crimes/other_details_screen.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   "/": (BuildContext context) => const HomeScreen(),
@@ -28,5 +41,16 @@ final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   "/InPublic": (BuildContext context) => const InPublicScreen(),
   "/Recognize": (BuildContext context) => const RecognizeScreen(),
   "/WithAnyone": (BuildContext context) => const WithAnyoneScreen(),
+
+  "/CommonCrime": (BuildContext context) => const WhatHappenedScreen(),
+  "/WhenItHappened": (BuildContext context) => const WhenItHappenedScreen(),
+  "/WhereItHappened": (BuildContext context) => const WhereItHappenedScreen(),
+  "/SeePerpetrator": (BuildContext context) => const SeePerpetratorScreen(),
+  "/Wearing": (BuildContext context) => const WearingScreen(),
+  "/ManOrWoman": (BuildContext context) => const ManOrWomanScreen(),
+  "/Age": (BuildContext context) => const AgeScreen(),
+  "/Height": (BuildContext context) => const HeightScreen(),
+  "/Race": (BuildContext context) => const RaceScreen(),
+  "/OtherDetails": (BuildContext context) => const OtherDetailsScreen(),
 
 };
