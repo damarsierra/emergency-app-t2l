85289e3d7b16bec536be5318a9e730840de9507e
Damar Sierra <dsierra7769@floridapoly.edu>
2021-11-29 02:14:08 -0500

    Updated slider so it works without error (Jasmine)

--- flutter_app/lib/screens/common_crimes/age_screen.dart
+++ flutter_app/lib/screens/common_crimes/age_screen.dart
@@ -31,58 +31,61 @@ class _AgeScreen extends State<AgeScreen> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Row(
           children: <Widget>[
-            const Padding(
-              padding: EdgeInsets.only(left: 32, right: 32, bottom: 16),
-              child: Text(
-                'How old are they?',
-                style: TextStyle(
-                    color: Color.fromRGBO(226, 226, 226, 30),
-                    fontWeight: FontWeight.w700),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: Slider(
-                value: _currentSliderValue,
-                min: 0,
-                max: 20,
-                divisions: 20,
-
-                activeColor: Colors.grey[600],
-                inactiveColor: Colors.grey[600],
-                thumbColor: Colors.grey[700],
-                label: _currentSliderValue.round().toString(),
-                onChanged: (double value) {
-                  setState(() {
-                    _currentSliderValue = value;
-                  });
-                },
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(top: 60),
-                  child: Container(
-                    decoration: boxDecoration(),
-                    child: OutlinedButton(
-                      style: buttonStyle(),
-                      onPressed: () {
-                        addToReport(_currentSliderValue);
-                        Navigator.pushNamed(context, '/Height');
-                      },
-                      child: const Text(
-                        'Submit',
-                        textAlign: TextAlign.center,
-                      ),
+            Flexible(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Padding(
+                    padding: EdgeInsets.only(left: 32, right: 32, bottom: 16),
+                    child: Text(
+                      'How old are they?',
+                      style: TextStyle(
+                          color: Color.fromRGBO(226, 226, 226, 30),
+                          fontWeight: FontWeight.w700),
                     ),
                   ),
-                ),
-              ],
+                  Slider(
+                    value: _currentSliderValue,
+                    min: 18,
+                    max: 70,
+                    divisions: 52,
+
+                    activeColor: Colors.grey[600],
+                    inactiveColor: Colors.grey[600],
+                    thumbColor: Colors.grey[700],
+                    label: _currentSliderValue.round().toString(),
+                    onChanged: (double value) {
+                      setState(() {
+                        _currentSliderValue = value;
+                      });
+                    },
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 60),
+                        child: Container(
+                          decoration: boxDecoration(),
+                          child: OutlinedButton(
+                            style: buttonStyle(),
+                            onPressed: () {
+                              addToReport(_currentSliderValue);
+                              Navigator.pushNamed(context, '/Height');
+                            },
+                            child: const Text(
+                              'Submit',
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
